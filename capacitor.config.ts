ba4a97b3e8cb6e4c2195ab6c9411966cb4dfb352
Andrew Moscardino <andrew@moscardino.net>
2023-01-05 13:33:55 -0500

    Initial cleanup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.moscardino.LinkThing',
   appName: 'LinkThing',
   webDir: 'build',
   bundledWebRuntime: false
