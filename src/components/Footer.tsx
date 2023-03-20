b4f54d8d9223899504ddd0993487308e214780e1
Andrew Moscardino <andrew@moscardino.net>
2023-03-20 10:17:42 -0400

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import { IonIcon, IonItem, IonLabel } from "@ionic/react";
-import { snowOutline } from "ionicons/icons";
+import { sparklesOutline } from "ionicons/icons";
 
 const Footer = () => (
     <IonItem lines="none">
         <IonLabel className="ion-text-center ion-padding-top ion-margin-top ion-margin-bottom">
-            <IonIcon icon={snowOutline} color="medium" style={{ fontSize: '3rem' }} />
+            <IonIcon icon={sparklesOutline} color="medium" style={{ fontSize: '3rem' }} />
         </IonLabel>
     </IonItem>
 );
