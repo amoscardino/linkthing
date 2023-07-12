1c9b32a6be6ba0a3a2ab27f0718c4c5ccd00d3f8
Andrew Moscardino <andrew@moscardino.net>
2023-07-12 13:24:32 -0400

    Fixed tag group sorting. Closes #32

--- src/hooks/useGroupedTags.ts
+++ src/hooks/useGroupedTags.ts
@@ -13,16 +13,21 @@ interface UseGroupedTagsResult extends QueryResult {
 const useGroupedTags = (): UseGroupedTagsResult => {
     const { tags, isSuccess, isLoading, isError } = useTags();
 
-    const groups = tags.reduce((groups: TagGroup[], tag: string) => {
-        const groupName = /[A-Z]/i.test(tag[0]) ? tag[0].toUpperCase() : '#';
-        const existingGroups = groups.filter(g => g.name === groupName);
+    const groups = tags
+        .reduce((groups: TagGroup[], tag: string) => {
+            const groupName = /[A-Z]/.test(tag[0].toUpperCase()) ? tag[0].toUpperCase() : '#';
+            const existingGroups = groups.filter(g => g.name === groupName);
 
-        if (!existingGroups.length)
-            return [...groups, { name: groupName, tags: [tag] }];
+            if (!existingGroups.length)
+                return [...groups, { name: groupName, tags: [tag] }];
 
-        existingGroups[0].tags.push(tag);
-        return [...groups];
-    }, [] as TagGroup[]);
+            existingGroups[0].tags.push(tag);
+            return [...groups];
+        }, [] as TagGroup[])
+        .sort((a, b) => a.name === "#" ? -1 : a.name.localeCompare(b.name));
+
+    for (let group of groups)
+        group.tags.sort((a, b) => a.localeCompare(b));
 
     return {
         groups,
